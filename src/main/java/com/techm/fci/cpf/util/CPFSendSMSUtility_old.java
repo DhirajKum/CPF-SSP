bdf5c8b6dbbd79ddc3068e3d5c159ae0106ee587
Dhiraj Kumar <dhiraj.4767@gmail.com>
2023-01-03 12:58:46 +0530

    Logger statement add

--- src/main/java/com/techm/fci/cpf/util/CPFSendSMSUtility_old.java
+++ src/main/java/com/techm/fci/cpf/util/CPFSendSMSUtility_old.java
@@ -6,31 +6,28 @@ import java.io.*;
 import java.util.logging.*;
 import java.sql.*;
 
-public class CPFSendSMSUtility
+public class CPFSendSMSUtility_old
 {
     public static void main(final String[] args) {
         String inputLine2 = "";
         try {
-        	System.out.println("::::: CPF Self Service Module ::::: Inside CPFSendSMSUtility jar ::::: Main Method ::::");
-
-        	Class.forName("oracle.jdbc.xa.client.OracleXADataSource");
-            final Connection con = DriverManager.getConnection("jdbc:oracle:thin:@10.248.225.38:1521/PROD", "fcipayroll", "qwerty#0786");
-            System.out.println("::::: CPF Self Service Module ::::: Inside CPFSendSMSUtility jar ::::: Connection Created ::::");
-            
+            System.out.println("Inside main method");
+            Class.forName("oracle.jdbc.xa.client.OracleXADataSource");
+            final Connection con = DriverManager.getConnection("jdbc:oracle:thin:@10.248.225.74:1521:TEST1", "fcipayroll", "orclnov16");
             final Statement stmt = con.createStatement();
             final Statement stmt2 = con.createStatement();
             ResultSet rs = null;
             ResultSet rs2 = null;
             rs = stmt.executeQuery("select PIN,MESSAGE,mobile_no,signature,dlt_entity_id,dlt_template_id,DEL_BY_SMSPORTAL from fcipayroll.cpf_otp_mas where nvl(del_by_smsportal,'N')='N'");
-            System.out.println("::::: CPF Self Service Module ::::: Inside CPFSendSMSUtility jar ::::: Result set size : "+rs.getFetchSize());
+            System.out.println("Result set size:");
             while (rs.next()) {
                 inputLine2 = "";
+                System.out.println("Inside while loop");
                 final String ValidationMessage = DataValidation(rs);
                 if (ValidationMessage.equalsIgnoreCase("")) {
-                    
+                    System.out.println("Inside DataValidation if");
                     final String httpsURL = "https://smsgw.sms.gov.in/failsafe/HttpLink?username=fcilekha.sms&pin=" + rs.getString(1) + "&message=" + rs.getString(2) + "&mnumber=" + rs.getString(3) + "&signature=" + rs.getString(4) + "&dlt_entity_id=" + rs.getString(5) + "&dlt_template_id=" + rs.getString(6);
-                    System.out.println("::::: CPF Self Service Module ::::: Inside CPFSendSMSUtility jar ::::: Url Created : " + httpsURL);
-                    
+                    System.out.println("Url Created: " + httpsURL);
                     final URL myUrl = new URL(httpsURL);
                     final HttpsURLConnection conn = (HttpsURLConnection)myUrl.openConnection();
                     final InputStream is = conn.getInputStream();
@@ -41,7 +38,6 @@ public class CPFSendSMSUtility
                         inputLine2 += inputLine3;
                         System.out.println(inputLine2);
                     }
-                    System.out.println("::::: CPF Self Service Module ::::: OTP Send Successfully ::::");
                     br.close();
                     if (inputLine2.contains("Accepted")) {
                         rs2 = stmt2.executeQuery("update fcipayroll.cpf_otp_mas set del_by_smsportal='Y',msg_status='Accepted', api_response='" + inputLine2 + "' where mobile_no = " + rs.getString(3));
@@ -82,7 +78,7 @@ public class CPFSendSMSUtility
             }
         }
         catch (SQLException ex) {
-            Logger.getLogger(CPFSendSMSUtility.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CPFSendSMSUtility_old.class.getName()).log(Level.SEVERE, null, ex);
         }
         return error_msg;
     }
